837fc8324a3a653c26d287d6ef390691b966a72b
presleyc95 <presleyc95@gmail.com>
2025-09-20 00:27:35 -0500

    Player movement and asteroid spawning

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroid_field import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -15,8 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for drawing in drawable:
+            drawing.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
