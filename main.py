a7eaeb2d804d9b3bc920488f63fca88be56dbc12
presleyc95 <presleyc95@gmail.com>
2025-09-19 20:20:20 -0500

    Pushing main.py with changes made based on boot.dev solution

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import pygame
 
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("blue")
+
+        screen.fill("black")
         pygame.display.flip()
 
 
