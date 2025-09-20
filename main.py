9b5e28ab9e892aee8218cc6ba85969a4a35b2ef2
presleyc95 <presleyc95@gmail.com>
2025-09-19 20:16:37 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def main():
+    pygame.init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("blue")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
